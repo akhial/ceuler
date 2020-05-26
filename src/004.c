c3081e1fa6b6858d1ed26633cc3c8ca66af2d97e
Adel Khial <adel.khial@gmail.com>
2020-05-26 13:45:18 +0100

    Add problem 4

---
+++ src/004.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char *);
+
+int main(void) {
+    int r = 0;
+    int a, b;
+
+    for(a = 100; a < 1000; a++) {
+        for(b = 100; b < 1000; b++) {
+            char s[7];
+            int p = a*b;
+
+            sprintf(s, "%d", p);
+
+            if(p > r && isPalindrome(s)) {
+                r = p;
+            }
+        }
+    }
+
+    printf("%d\n", r);
+
+    return 0;
+}
+
+int isPalindrome(char * n) {
+    int l = strlen(n);
+
+    if(0 != l%2) {
+        return 0;
+    }
+
+    char p[l/2];
+    char s[l/2];
+    int i;
+
+    for(i = 0; i < l/2; i++) {
+        p[i] = n[i];
+        s[i] = n[l - 1 - i];
+    }
+
+    return 0 == strncmp(p, s, l/2);
+}
+
