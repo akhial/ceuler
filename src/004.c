db8ebd15fe2c7bc8fd3f47b691d64073a3b2ec51
Adel Khial <adel.khial@gmail.com>
2020-05-29 04:12:40 +0100

    Fix code style

--- src/004.c
+++ src/004.c
@@ -7,14 +7,14 @@ int main(void) {
     int r = 0;
     int a, b;
 
-    for(a = 100; a < 1000; a++) {
-        for(b = 100; b < 1000; b++) {
+    for (a = 100; a < 1000; a++) {
+        for (b = 100; b < 1000; b++) {
             char s[7];
             int p = a*b;
 
             sprintf(s, "%d", p);
 
-            if(p > r && isPalindrome(s)) {
+            if (p > r && isPalindrome(s)) {
                 r = p;
             }
         }
@@ -28,19 +28,18 @@ int main(void) {
 int isPalindrome(char * n) {
     int l = strlen(n);
 
-    if(0 != l%2) {
+    if (0 != l%2) {
         return 0;
     }
 
-    char p[l/2];
-    char s[l/2];
     int i;
 
-    for(i = 0; i < l/2; i++) {
-        p[i] = n[i];
-        s[i] = n[l - 1 - i];
+    for (i = 0; i < l/2; i++) {
+        if (n[i] != n[l - 1 - i]) {
+            return 0;
+        }
     }
 
-    return 0 == strncmp(p, s, l/2);
+    return 1;
 }
 
